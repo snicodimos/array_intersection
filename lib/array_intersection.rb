fa95d3d58832ae40bb96b41fd9a786a53ad74d7e
Semret Nicodimos <snicodimos@gmail.com>
2018-10-22 23:04:21 -0700

    added method to find intersection and passed all tests

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,20 @@
 # Creates a new array to return the intersection of the two input arrays
 def intersection(array1, array2)
-  raise NotImplementedError
+
+  intersect = []
+  if array1 == nil || array2 == nil
+    return intersect
+  end
+
+  array1.each do |num1|
+    array2.each do |num2|
+      if num1 == num2
+        intersect << num1
+      end
+    end
+  end
+
+  return intersect
 end
+
+# Time complexity is quadratic and space complexity is O(n)
